e7cb91bf6d49be142dabc93bda28604db07ccf35
priscolatrans <priscolatrans@gmail.com>
2022-05-11 00:37:58 +0900

    Feat: 메인페이지에 GNB, Tabbar 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Test from '@/components/TestComponent';
 import styled from 'styled-components';
+import Tabbar from '@/components/tabbar';
+import Gnb from '@/components/gnb';
 
 const HomeWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 80vh;
+  /* display: flex; */
+  /* justify-content: center; */
+  /* align-items: center; */
+  /* height: 80vh; */
+  /* height: 100vh; */
+  /* width: 100%; */
+  /* padding: 0 24px; */
 `;
 
 const Home = () => {
   return (
     <HomeWrap>
-      <Test />
+      <Gnb />
+      <Tabbar />
     </HomeWrap>
     )
 }
