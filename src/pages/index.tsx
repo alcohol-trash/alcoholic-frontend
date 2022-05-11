b4377eda9a7273c14fe4b69e85751ed9841ed667
bluewe9 <85469867+bluewe9@users.noreply.github.com>
2022-05-11 09:15:04 +0900

    Feat: 메인페이지에 카테고리 더미 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Test from '@/components/TestComponent';
 import styled from 'styled-components';
 import Tabbar from '@/components/tabbar';
 import Gnb from '@/components/gnb';
+import NoticeTitle from '@/components/noticetitle';
+import Category from '@/components/category';
 
 const HomeWrap = styled.div`
   /* display: flex; */
@@ -14,13 +16,45 @@ const HomeWrap = styled.div`
   /* padding: 0 24px; */
 `;
 
+const IntroductionContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding-left: 24px;
+`;
+
+const CategoryContainer = styled.div`
+  display: flex;
+  margin-top: 12px;
+  div {
+    margin-right: 8px;
+  }
+
+`;
+
 const Home = () => {
   return (
-    <HomeWrap>
+    <IntroductionContainer>
       <Gnb />
+      <NoticeTitle title="주류학개론" description='술에 대한 정보, 리뷰를 올려주세요.' />
+      <CategoryContainer>
+        {CATEGORY_DUMMY.map((data) =>
+          <Category content={data.content} key={data.id} />
+        )}
+      </CategoryContainer>
       <Tabbar />
-    </HomeWrap>
-    )
+    </IntroductionContainer>
+  )
 }
 
 export default Home;
+
+const CATEGORY_DUMMY = [
+  {
+    id: 1,
+    content: '최신순'
+  },
+  {
+    id: 1,
+    content: '추천순'
+  },
+]
