ad5f5479607a0923b871a7cb942fe0156e10a23a
priscolatrans <priscolatrans@gmail.com>
2022-05-14 11:29:08 +0900

    Feat: 피드에 푸터 추가

--- src/components/feed/index.tsx
+++ src/components/feed/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
 
+import Category from '@/components/category';
 import ProfileDefault from '@/public/assets/profile_default.png';
 import Babamba from '@/public/assets/babamba.png';
 
-const FeedContainer = styled.section``;
+const FeedContainer = styled.section`
+  padding-bottom: 20px;
+`;
 
 const FeedHeader = styled.div`
   display: flex;
@@ -41,18 +44,40 @@ const FeedContentDescription = styled.p`
 `;
 const FeedContentImage = styled.div`
   padding-top: 12px;
-  
   img {
     border-radius: 10px;
     width: 100%;
   }
 `;
 
-const FeedFooter = styled.div``;
+const FeedFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 const FeedHash = styled.div``;
-const FeedComment = styled.div``;
+const FeedComment = styled.div`
+  padding-top: 20px;
+  padding-right: 4px;
+  p {
+    font-weight: 400;
+    strong {
+      font-weight: 700;
+    }
+  }
+`;
+
+const CategoryContainer = styled.section`
+  display: flex;
+  margin-top: 12px;
+  div {
+    margin-right: 8px;
+  }
+`;
 
 const Feed = () => {
+  const [cheers, set_cheers] = useState(1);
+  const [clink, set_clink] = useState(1);
+
   return (
     <FeedContainer>
       <FeedHeader>
@@ -73,9 +98,21 @@ const Feed = () => {
           신상 바밤바 막걸리! 아이스크림 맛이에요 ㅎ.ㅎ
         </FeedContentDescription>
         <FeedContentImage>
-          <Image src={Babamba} alt="바밤바 캔 막걸리" layout='responsive'/>
+          <Image src={Babamba} alt="바밤바 캔 막걸리" layout='responsive' />
         </FeedContentImage>
       </FeedContent>
+
+      <FeedFooter>
+        <FeedHash>
+          <CategoryContainer>
+            <Category content={'🍻'} count={cheers} onClick={() => set_cheers((cheers) => cheers+1)} />
+            <Category content={'짠!'} count={clink} onClick={() => set_clink((clink) => clink+1)} />
+          </CategoryContainer>
+        </FeedHash>
+        <FeedComment>
+          <p>댓글 <strong>3</strong>개</p>
+        </FeedComment>
+      </FeedFooter>
     </FeedContainer>
   )
 }
