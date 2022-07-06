973b3a7eae7892115e6f18cf40d3df46643fc4f4
hyejinlim <yoon1440_@naver.com>
2022-07-06 10:03:47 +0900

    feat: emotion + example

---
+++ src/pages/example/index.tsx
@@ -0,0 +1,8 @@
+/** @jsxImportSource @emotion/react */
+import * as styles from './styles'
+
+const Example = () => {
+  return <div css={styles.container}>텍스트</div>
+}
+
+export default Example
---
+++ src/pages/example/styles.ts
@@ -0,0 +1,4 @@
+import { css } from '@emotion/react'
+export const container = css`
+  color: #ff0000;
+`
