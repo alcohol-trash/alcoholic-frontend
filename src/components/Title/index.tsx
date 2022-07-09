4130ea40a6780ba055ffa893c33ddc1bf3bfa63f
hyejinlim <yoon1440_@naver.com>
2022-07-09 13:38:40 +0900

    fix: React.memo 추가

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import * as styles from './styles'
 
 type Props = {
@@ -11,4 +12,4 @@ const Title = ({ children }: Props) => {
   )
 }
 
-export default Title
+export default React.memo(Title)
