7f28ffd699f07c4d2ca18069f80ffc4eb5d2cec0
YoonSeoRoh <yoonseo1997@naver.com>
2022-07-19 18:32:21 +0900

    feat: 설정 UI

--- src/pages/setting/setting-main/index.tsx
+++ src/pages/setting/setting-main/index.tsx
@@ -1,15 +1,21 @@
+import Router from 'next/router'
+
 import Header from '@/components/Header'
 import * as styles from './styles'
 
 const Setting = () => {
+  const handleLogout = () => {
+    Router.push('/')
+  }
   return (
     <section css={styles.container}>
       <Header title="설정" />
-      <ul>
+      <ul css={styles.list}>
         <li>계정 정보</li>
-        <li>프로필 편집</li>
+        <li css={styles.border}>프로필 편집</li>
         <li>고객센터</li>
-        <li>이용약관</li>
+        <li css={styles.border}>이용약관</li>
+        <li onClick={handleLogout}>로그아웃</li>
       </ul>
     </section>
   )
--- src/pages/setting/setting-main/styles.ts
+++ src/pages/setting/setting-main/styles.ts
@@ -1,5 +1,15 @@
 import { css } from '@emotion/react'
+import theme from '@/theme'
 
 export const container = css`
   padding: 0 20px;
+`
+export const list = css`
+  li {
+    margin-bottom: 5px;
+    padding: 20px 0;
+  }
+`
+export const border = css`
+  border-bottom: 1px solid ${theme.gray[700]};
 `
