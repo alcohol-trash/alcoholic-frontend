4cffbae571aa408cd832655bf39e672ddfc76101
YoonSeoRoh <yoonseo1997@naver.com>
2022-07-26 14:55:28 +0900

    fix: 배포 에러

--- src/pages/setting/setting-terms/index.tsx
+++ src/pages/setting/setting-terms/index.tsx
@@ -4,7 +4,7 @@ import * as styles from '@/css/setting/settingTermsStyles'
 const Terms = () => {
   return (
     <section css={styles.container}>
-      <Header title="이용약관" style="left" />
+      <Header title="이용약관" style="default" />
       <ul css={styles.list}>
         <li>서비스 이용약관</li>
         <li>개인정보 처리방침</li>
