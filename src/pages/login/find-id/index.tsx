dbb8a5e3b705e95d97233dc1911805a5cae7f3a7
hyejinlim <yoon1440_@naver.com>
2022-07-31 16:11:46 +0900

    feat: 아이디 찾기 이메일 인증 API 연동

--- src/pages/login/find-id/index.tsx
+++ src/pages/login/find-id/index.tsx
@@ -9,9 +9,11 @@ import TextField from '@/components/TextField'
 import Button from '@/components/Button'
 import ValidateMessage from '@/components/ValidateMessage'
 import AuthTimer from '@/components/AuthTimer'
+import ModalAlert from '@/components/ModalAlert'
 
 import * as styles from '@/css/login/findIdStyles'
 
+const MAIL_TYPE = 'id'
 type FormTypes = {
   email: string
 }
@@ -19,6 +21,7 @@ const FindId = () => {
   const {
     register,
     setValue,
+    getValues,
     handleSubmit,
     formState: { isValid, errors }, // isValid: Set to true if the form doesn't have any errors.
   } = useForm<FormTypes>({
@@ -26,27 +29,45 @@ const FindId = () => {
   })
   const [checkDisabled, setCheckDisabled] = useState<boolean>(true)
   const [time, setTime] = useState<number>(5)
+  const [modalVisible, setModalVisible] = useState<boolean>(false)
+  const [modalTitle, setModalTitle] = useState<string>('')
 
   const handleChange = ({ name, value }: any) => {
     // TODO: any 타입 변경
     setValue(name, value, { shouldValidate: true })
   }
-  const handleClick = (data: FormTypes) => {
-    // TODO: 인증 요청 API
-    console.log('인증 요청 클릭', data)
+  const handleClick = async (formData: FormTypes) => {
+    const { email } = formData
 
-    // 인증 확인 disabled -> default
-    setCheckDisabled(false)
+    // const response = await fetch(`/api/email/send/${MAIL_TYPE}?email=${email}`)
+    // const data = await response.json()
+    const data = { success: true, message: '123' }
+    if (data) {
+      setModalVisible(true)
+      setModalTitle(data.message)
 
-    if (!checkDisabled) {
-      // TODO: 재요청
-      setTime(5)
+      if (data.success) {
+        setCheckDisabled(false) // 인증 확인 disabled -> default
+        // 재요청
+        if (!checkDisabled) {
+          setTime(5)
+        }
+      }
     }
   }
 
-  const handleCheckClick = () => {
-    // TODO: 인증 확인 API -> 아이디 찾기 결과
-    Router.push('/login/find-id/success')
+  const handleCheckClick = async () => {
+    const email = getValues('email')
+    const response = await fetch(`/api/email/check/${MAIL_TYPE}?email=${email}`)
+    const data = await response.json()
+    if (data) {
+      setModalVisible(true)
+      setModalTitle(data.message)
+
+      if (data.success) {
+        Router.push('/login/find-id/success')
+      }
+    }
   }
 
   return (
@@ -103,6 +124,12 @@ const FindId = () => {
           인증 확인
         </Button>
       </div>
+
+      <ModalAlert
+        isOpen={modalVisible}
+        title={modalTitle}
+        onClick={() => setModalVisible(!modalVisible)}
+      />
     </>
   )
 }
