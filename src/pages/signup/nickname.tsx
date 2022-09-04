46b57a34239a1c061ae3a402cbb74271dbf12cf9
YoonSeoRoh <yoonseo1997@naver.com>
2022-09-04 22:01:26 +0900

    fix: 오류 수정 중

--- src/pages/signup/nickname.tsx
+++ src/pages/signup/nickname.tsx
@@ -1,5 +1,5 @@
 //소셜 회원가입 -> 닉네임 생성 페이지
-import NicknameForm from '@/components/Temp'
+import NicknameForm from '@/components/NicknameForm'
 import Sentence from '@/components/Sentence'
 import Title from '@/components/Title'
 
