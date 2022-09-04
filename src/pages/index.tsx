6444645ceecbf062bbb8108192f7460a06f450c2
YoonSeoRoh <yoonseo1997@naver.com>
2022-09-04 21:57:31 +0900

    fix: 오류 수정 중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router'
 
 import Title from '@/components/Title'
 import Sentence from '@/components/Sentence'
-import Gnb from '@/components/Temp'
+import Gnb from '@/components/Gnb'
 import Tabs from '@/components/Tabs'
 import Feed from '@/components/Feed'
 import BottomBar from '@/components/BottomBar'
