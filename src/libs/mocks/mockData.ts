0e84f8b6ceb62347177027d1a3c68a81119267a0
hyejinlim <yoon1440_@naver.com>
2022-09-04 12:43:47 +0900

    feat: 컨텐츠 mock data

--- src/libs/mocks/mockData.ts
+++ src/libs/mocks/mockData.ts
@@ -59,3 +59,43 @@ export const menus = [
   { name: '내가 쓴 글', count: mockWriteData?.length },
   { name: '내가 쓴 댓글', count: mockCommentData?.length },
 ]
+
+/**
+ * 컨텐츠 리스트, 진입: /content-detail/{id}
+ */
+export const contentData = [
+  {
+    id: 1,
+    title: '오늘 집들이!1',
+    content: '123123123',
+    isLike: true,
+    like: 3,
+    created: '2022.07.01T14:54:22',
+    updated: '2022.07.02T14:54:22',
+    writer: '1000 병의 소주를 마신 아저씨',
+  },
+  {
+    id: 2,
+    title: '오늘 집들이!2',
+    content: '123123123',
+    isLike: false,
+    like: 3,
+    created: '2022.07.01T14:54:22',
+    updated: '2022.07.02T14:54:22',
+    writer: '1000 병의 소주를 마신 아저씨',
+  },
+]
+
+export const commentData = [
+  {
+    contentId: 1,
+    data: [
+      {
+        userId: 1,
+        content: '오 너무 좋은 글이네요ㅎㅎ 잘봤습니다!',
+        created: '2022.07.03T14:54:22',
+        updatedDate: 'string',
+      },
+    ],
+  },
+]
