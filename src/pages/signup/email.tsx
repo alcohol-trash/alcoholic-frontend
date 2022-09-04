d1fb278ee201675987ffc522dedd1cc9ce543c4d
YoonSeoRoh <yoonseo1997@naver.com>
2022-09-04 21:49:23 +0900

    fix: 오류 수정 중

--- src/pages/signup/email.tsx
+++ src/pages/signup/email.tsx
@@ -1,5 +1,5 @@
 //일반 회원가입 -> 이메일 인증
-import EmailForm from '@/components/EmailForm'
+import EmailForm from '@/components/Temp'
 import Title from '@/components/Title'
 
 import * as styles from '@/css/signup/signupEmailStyles'
