b67e86f56ba6483a73b9db22a11f10894d093bb2
YoonSeoRoh <yoonseo1997@naver.com>
2022-09-06 23:54:50 +0900

    fix: auth api 수정

--- src/pages/api/auth/[type].ts
+++ src/pages/api/auth/[type].ts
@@ -5,7 +5,6 @@ export default async function Auth(req: NextApiRequest, res: NextApiResponse) {
   const { type } = req.query
   const response = await fetch(`${apiBaseUrl}/api/auth/${type}`, {
     method: 'POST',
-    credentials: 'same-origin',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
@@ -13,9 +12,10 @@ export default async function Auth(req: NextApiRequest, res: NextApiResponse) {
     body: req.body,
   })
   const data = await response.json()
-  const token = req.headers.cookie
-  //const token = response.headers.get('Set-Cookie')
-  res.setHeader('Set-Cookie', `${token}; path=/;`)
+  if (type === 'login') {
+    const token = response.headers.get('Set-Cookie')
+    res.setHeader('Set-Cookie', `${token}; path=/;`)
+  }
   if (type === 'logout') {
     res.setHeader('Set-Cookie', `; path=/; expires=-1`)
   }
