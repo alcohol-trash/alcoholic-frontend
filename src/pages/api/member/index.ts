bc310fd55d169993eaa305760657d335f812e914
YoonSeoRoh <yoonseo1997@naver.com>
2022-09-07 00:13:13 +0900

    fix: member api

--- src/pages/api/member/index.ts
+++ src/pages/api/member/index.ts
@@ -2,7 +2,11 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { apiBaseUrl } from '@/libs/config'
 
 export async function MemberInfo(req: NextApiRequest, res: NextApiResponse) {
-  const response = await fetch(`${apiBaseUrl}/api/member`)
+  const response = await fetch(`${apiBaseUrl}/api/member`, {
+    headers: {
+      cookie: `${document.cookie}`,
+    },
+  })
   const data = await response.json()
   res.status(response.status).json(data)
 }
