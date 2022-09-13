0f0a2a79cfd3f04268fb05656be300720af439b8
YoonSeoRoh <yoonseo1997@naver.com>
2022-09-13 23:18:40 +0900

    fix: content css파일 분리

--- src/pages/content/detail/[id].tsx
+++ src/pages/content/detail/[id].tsx
@@ -7,7 +7,7 @@ import Header from '@/components/Header'
 import BackButton from '@/components/BackButton'
 import Profile from '@/components/Profile'
 
-import * as styles from './styles'
+import * as styles from '@/css/content/contentDetailStyles'
 
 const ContentDetail = () => {
   const router = useRouter()
