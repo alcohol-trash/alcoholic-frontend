f0512a1550af8ab37d8a7e93c44081ffd62e93f6
YoonSeoRoh <yoonseo1997@naver.com>
2022-09-18 02:33:00 +0900

    test: 테스트 코드 수정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   images: {
     loader: 'akamai',
     path: '/',
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `https://api.alcoholic.ml/:path*`,
+      },
+    ]
   },
 }
