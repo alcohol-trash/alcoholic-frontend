fcece312208c58bc29222667fed8883b052bc83c
YoonSeoRoh <yoonseo1997@naver.com>
2022-09-21 01:14:27 +0900

    feat: add credentials

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,6 +2,7 @@ import axios, { AxiosInstance } from 'axios'
 
 const instance: AxiosInstance = axios.create({
   baseURL: 'https://api.alcoholic.ml',
+  withCredentials: true,
   headers: {
     Accept: '*/*',
     'Content-Type': 'application/json',
